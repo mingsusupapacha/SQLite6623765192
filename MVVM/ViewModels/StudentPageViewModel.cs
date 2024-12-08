0d1ddbcb9e8650d54a2d7155f314981224075168
supacha vanichayanan <[email protected]>
2024-12-08 21:39:57 +0700

    first commit

---
+++ MVVM/ViewModels/StudentPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Input;
+using PropertyChanged;
+using SQLiteDemo.MVVM.Models;
+
+namespace SQLiteDemo.MVVM.ViewModels;
+
+[AddINotifyPropertyChangedInterface]
+public class StudentPageViewModel
+{
+    public List<Student> Students { get; set; }
+    public Student CurrentStudent { get; set; }
+    public ICommand AddOrUpdateCommand { get; set; }
+    public ICommand DeleteCommand { get; set; }
+    public StudentPageViewModel()
+    {
+        this.Refresh();
+        AddOrUpdateCommand = new Command(async () =>
+        {
+            App.StudentRepo.AddOrUpdate(CurrentStudent);
+            Console.WriteLine(App.StudentRepo.StatusMessage);
+            this.Refresh();
+        });
+
+        DeleteCommand = new Command(async () => {
+            App.StudentRepo.Delete(CurrentStudent.ID);
+            this.Refresh();
+        });
+    }
+    private void Refresh()
+    {
+        CurrentStudent = new Student();
+
+        this.Students = App.StudentRepo.GetAll();
+    }
+}
